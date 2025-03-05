53ffc9a1970753e3a6fccd1b8a7a115e6f8b7231
Moustapha00864 <moustapha00864@gmail.com>
2025-03-05 16:52:09 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI, File, UploadFile, HTTPException
+import io
+import cv2
+import numpy as np
+from keras.preprocessing import image as i1
+from keras import models
+
+app = FastAPI()
+
+# Charger le modèle au démarrage de l'application
+model = models.load_model("model.h5")
+
+# Fonction de classification
+def predict_label(img_bytes):
+    # Conversion de l'image en tableau NumPy
+    img = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), cv2.IMREAD_COLOR)
+    if img is None:
+        raise HTTPException(status_code=400, detail="Invalid image format")
+
+    resized = cv2.resize(img, (50, 50))  # Redimensionnement à 50x50
+    img_array = i1.img_to_array(resized) / 255.0  # Normalisation
+    img_array = img_array.reshape(1, 50, 50, 3)  # Ajustement de la forme pour le modèle
+
+    result = model.predict(img_array)
+    a = round(result[0, 0], 2) * 100
+    b = round(result[0, 1], 2) * 100
+    probability = [a, b]
+    threshold = 10
+
+    if a > threshold or b > threshold:
+        ind = np.argmax(result)
+        classes = ["Cellule Normal: Pas de Paludisme", "Cellule Infectée: Présence du Paludisme"]
+        return classes[ind], probability[ind]
+    else:
+        return "Image invalide", 0
+
+# Endpoint pour l'upload et la classification
+@app.post("/predict/")
+async def classify_image(file: UploadFile = File(...)):
+    try:
+        # Lecture du fichier en mémoire
+        img_bytes = await file.read()
+
+        # Classification
+        label, probability = predict_label(img_bytes)
+        return {"filename": file.filename, "label": label, "probability": probability}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# Route d'accueil
+@app.get("/")
+def home():
+    return {"message": "Bienvenue sur l'API de classification du paludisme"}
